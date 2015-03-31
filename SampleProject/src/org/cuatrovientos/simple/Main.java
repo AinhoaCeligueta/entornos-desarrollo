2610e3440529d47539d4a8e7ee8549d49e7703d5
Ainhoa <Ainhoa@Ainhoa>
2015-03-31 23:42:57 +0200

    First commit to repository

---
+++ SampleProject/src/org/cuatrovientos/simple/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.cuatrovientos.simple;
+
+/**
+ * @author Ainhoa Celigueta
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
